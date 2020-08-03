3a3b3b0af0869a3fd726945d2886f2e595096f24
bafiam <stevebafiam@gmail.com>
2020-08-04 01:13:41 +0300

    setup:configure hot reload at app

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './vetPlus/App';
 import * as serviceWorker from './serviceWorker';
 
+const renderApp = () => {
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+};
 
+renderApp();
+
+// Hot module reloading
+if (module.hot) {
+  module.hot.accept('./vetPlus/App', renderApp);
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/vetPlus/App.js
+++ src/vetPlus/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      number : 0
+    }
+  }
   
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+        Will the state be preserved? The value is {this.state.number}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button onClick={()=>this.setState({number : this.state.number + 1})}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
